3b43814b9419c835c367195cf7ffe5429a16a305
Chris Hendrix <chendrix1123@gmail.com>
2021-11-18 19:11:21 -0500

    exercise 1.7

--- part1.2-unicafe/src/App.js
+++ part1.2-unicafe/src/App.js
@@ -1,12 +1,48 @@
 import React, { useState } from 'react';
 
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>;
+
+const Stat = ({ name, value }) => (
+  <p>
+    {name} {value}
+  </p>
+);
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  const all = good + neutral + bad;
+  const score = 1 * good + 0 * neutral + -1 * bad;
+  const average = all === 0 ? 0 : score / all;
+  const positive = all === 0 ? '0%' : String((good / all) * 100) + '%';
+
+  const setFeedback = (feedback) => {
+    console.log(feedback);
+    feedback === 'good' && setGood(good + 1);
+    feedback === 'neutral' && setNeutral(neutral + 1);
+    feedback === 'bad' && setBad(bad + 1);
+  };
+
+  const feedbacks = ['good', 'neutral', 'bad'];
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      {feedbacks.map((f) => (
+        <Button handleClick={() => setFeedback(f)} text={f} />
+      ))}
+      <h1>statistics</h1>
+      <Stat name='good' value={good} />
+      <Stat name='neutral' value={neutral} />
+      <Stat name='bad' value={bad} />
+      <Stat name='all' value={all} />
+      <Stat name='average' value={average} />
+      <Stat name='positive' value={positive} />
+    </div>
+  );
 };
 
 export default App;
