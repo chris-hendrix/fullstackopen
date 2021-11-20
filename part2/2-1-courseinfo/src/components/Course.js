5c7af673aed1cef112eba5960aaff3558f7a41b0
Chris Hendrix <chendrix1123@gmail.com>
2021-11-20 13:38:53 -0500

    2.3

--- part2/2-1-courseinfo/src/components/Course.js
+++ part2/2-1-courseinfo/src/components/Course.js
@@ -1,25 +1,26 @@
 import React from 'react';
 
 const Course = ({ course }) => {
-  let total;
   const getParts = () => {
     {
-      total = 0;
       return course.parts.map((part) => {
-        total += part.exercises;
         return <li key={part.id}>{part.name + ' ' + part.exercises}</li>;
       });
     }
   };
-  console.log(getParts());
+  const getTotal = () => {
+    const total = course.parts.reduce((total, part) => total + part.exercises, 0);
+    return (
+      <li>
+        <strong>total of {total} exercises</strong>
+      </li>
+    );
+  };
   return (
     <div>
       <h1>{course.name}</h1>
       <ul>
-        {getParts()}
-        <li>
-          <strong>total of {total} exercises</strong>
-        </li>
+        {getParts()} {getTotal()}
       </ul>
     </div>
   );
