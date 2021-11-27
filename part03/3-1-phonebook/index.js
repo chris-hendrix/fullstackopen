949cdae2ad5df7f2bab925a5e7c8e75cbf6995d9
Chris Hendrix <chendrix1123@gmail.com>
2021-11-27 11:19:21 -0600

    3.2-3.6

--- part03/3-1-phonebook/index.js
+++ part03/3-1-phonebook/index.js
@@ -66,25 +66,25 @@ app.delete('/api/persons/:id', (request, response) => {
 });
 
 // POST new person
-const generateId = () => {
-  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
-
 app.post('/api/persons', (request, response) => {
   const body = request.body;
-
-  if (!body.content) {
+  // name and number missing
+  if (!body.name || !body.number) {
     return response.status(400).json({
       error: 'content missing',
     });
   }
-
+  // person exists
+  if (persons.filter((person) => person.name === body.name).length > 0) {
+    return response.status(400).json({
+      error: 'name must be unique',
+    });
+  }
   const person = {
-    content: body.content,
-    important: body.important || false,
+    name: body.name,
+    number: body.number,
     date: new Date(),
-    id: generateId(),
+    id: body.id || Math.floor(Math.random() * 999999),
   };
 
   persons = persons.concat(person);
