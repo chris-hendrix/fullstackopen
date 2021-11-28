43e314a51e993ec8fcfc9c223a9b627783fbf98f
Chris Hendrix <chendrix1123@gmail.com>
2021-11-28 12:39:56 -0600

    3.11

--- part03/3-1-phonebook/index.js
+++ part03/3-1-phonebook/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static('./client/build'));
 
 // morgan middleware
 morgan.token('id', function getId(req) {
@@ -111,6 +111,17 @@ app.post('/api/persons', (request, response) => {
 
   response.json(person);
 });
+
+console.log(process.env.NODE_ENV);
+// serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
