f418449eacb2a93704b76ed8fc56d03e60820066
Chris Hendrix <chendrix1123@gmail.com>
2021-12-21 16:46:46 -0600

    4.1,4.2

---
+++ part04/4-0-notes/models/note.js
@@ -0,0 +1,27 @@
+const dotenv = require('dotenv').config();
+const mongoose = require('mongoose');
+
+// note schema
+const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    minLength: 5,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  important: Boolean,
+});
+
+// converts id to string and deletes v from json results
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model('Note', noteSchema);
