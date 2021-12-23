668352134838b4f05c0c21c416e5643557360044
Chris Hendrix <chendrix1123@gmail.com>
2021-12-23 13:09:06 -0600

    part04d

--- part04/4-0-notes/server/controllers/notes.js
+++ part04/4-0-notes/server/controllers/notes.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const notesRouter = require('express').Router();
 const Note = require('../models/note');
 const User = require('../models/user');
@@ -8,9 +9,22 @@ notesRouter.get('/', async (request, response) => {
   response.json(notes.map((note) => note.toJSON()));
 });
 
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7);
+  }
+  return null;
+};
+
 notesRouter.post('/', async (request, response) => {
   const body = request.body;
-  const user = await User.findById(body.userId);
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
+  const user = await User.findById(decodedToken.id);
 
   const note = new Note({
     content: body.content,
