1d10a415e220f47a959bc4a9f68e85cb0d4fb7dd
Chris Hendrix <chendrix1123@gmail.com>
2021-12-24 13:50:32 -0600

    5.8

--- part05/5-1-bloglist/client/src/components/Blog.js
+++ part05/5-1-bloglist/client/src/components/Blog.js
@@ -1,8 +1,27 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React from 'react';
+import Togglable from './Togglable';
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
 
-export default Blog
+  const blogDetails = () => (
+    <div>
+      <p>
+        {blog.url} <br /> likes: {blog.likes}
+      </p>
+    </div>
+  );
+  return (
+    <div style={blogStyle}>
+      {blog.title} by {blog.author}
+      <Togglable buttonLabel='view'>{blogDetails()}</Togglable>
+    </div>
+  );
+};
+
+export default Blog;
