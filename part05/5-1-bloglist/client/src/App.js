5892c19ec8cd3a84ed24ed300b472ffc26b36c07
Chris Hendrix <chendrix1123@gmail.com>
2021-12-24 14:44:34 -0600

    5.9

--- part05/5-1-bloglist/client/src/App.js
+++ part05/5-1-bloglist/client/src/App.js
@@ -119,20 +119,23 @@ const App = () => {
     </Togglable>
   );
 
-  const blogList = () => (
-    <div>
-      <p>{user.name} is logged in</p>
-      <button onClick={handleLogout} type='button'>
-        Logout
-      </button>
-      <h2>create new</h2>
-      <div>{blogForm()}</div>
-      <h2>blogs</h2>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
-      ))}
-    </div>
-  );
+  const blogList = () => {
+    const sortedBlogs = [...blogs].sort((a, b) => (a.likes > b.likes ? -1 : 1));
+    return (
+      <div>
+        <p>{user.name} is logged in</p>
+        <button onClick={handleLogout} type='button'>
+          Logout
+        </button>
+        <h2>create new</h2>
+        <div>{blogForm()}</div>
+        <h2>blogs</h2>
+        {sortedBlogs.map((blog) => (
+          <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
+        ))}
+      </div>
+    );
+  };
 
   return (
     <div>
