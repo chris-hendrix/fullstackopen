000ed81c7b13ee97b24c8ced48bfbb5563c14c39
Chris Hendrix <chendrix1123@gmail.com>
2021-12-26 14:05:03 -0600

    6.9

--- part06/6-3-anecdotes/client/src/index.js
+++ part06/6-3-anecdotes/client/src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import store from './store';
 import App from './App';
-import reducer from './reducers/anecdoteReducer';
-
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 ReactDOM.render(
   <Provider store={store}>
