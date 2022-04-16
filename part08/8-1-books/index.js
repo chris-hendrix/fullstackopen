c0ed2576e4b61e59c7db8628894539c4e2b8b624
chendrix <chendrix1123@gmail.com>
2022-04-16 11:28:17 -0400

    8.1

--- part08/8-1-books/index.js
+++ part08/8-1-books/index.js
@@ -1,16 +1,32 @@
-// const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer, gql } = require('apollo-server')
 const bookData = require('./bookData.js')
 
 let books = bookData.books
 let authors = bookData.authors
 
 const typeDefs = gql`
+  type Author {
+    id: ID!
+    name: String!
+    born: Int!
+  }
+  type Book {
+    id: ID!
+    title: String!
+    published: Int!
+    author: Author!
+    genres: [String]
+  }
   type Query {
+    bookCount: Int!
+    authorCount: Int!
   }
 `
 
 const resolvers = {
   Query: {
+    bookCount: () => books.length,
+    authorCount: () => authors.length
   }
 }
 
@@ -21,5 +37,4 @@ const server = new ApolloServer({
 
 server.listen().then(({ url }) => {
   console.log(`Server ready at ${url}`)
-  console.log(bookData)
 })
