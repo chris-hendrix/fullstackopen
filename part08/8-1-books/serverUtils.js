b56a0994726cbb81a96eabf0cd0f628237b9f17d
chendrix <chendrix1123@gmail.com>
2022-04-17 21:01:48 -0400

    refactor books

--- part08/8-1-books/serverUtils.js
+++ part08/8-1-books/serverUtils.js
@@ -52,4 +52,15 @@ const resolvers = {
   }
 }
 
-module.exports = resolvers
+const context = async ({ req }) => {
+  const auth = req ? req.headers.authorization : null
+  if (auth && auth.toLowerCase().startsWith('bearer ')) {
+    const decodedToken = jwt.verify(
+      auth.substring(7), JWT_SECRET
+    )
+    const currentUser = await User.findById(decodedToken.id)
+    return { currentUser }
+  }
+}
+
+module.exports = { resolvers, context }
