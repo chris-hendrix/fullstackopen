fb463c395d4ff47689e2e9d6273bca6171360d4f
chendrix <chendrix1123@gmail.com>
2022-04-17 21:24:09 -0400

    8.23 setup

--- part08/8-1-books/index.js
+++ part08/8-1-books/index.js
@@ -1,6 +1,14 @@
-const { ApolloServer } = require('apollo-server')
-const mongoose = require('mongoose')
+const { ApolloServer } = require('apollo-server-express')
+const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core')
+const { makeExecutableSchema } = require('@graphql-tools/schema')
+const { execute, subscribe } = require('graphql')
+const { SubscriptionServer } = require('subscriptions-transport-ws')
+
 require('dotenv').config()
+const express = require('express')
+const http = require('http')
+const mongoose = require('mongoose')
+
 
 const typeDefs = require('./schema')
 const { resolvers, context } = require('./serverUtils')
@@ -18,12 +26,30 @@ mongoose
     console.log('error connection to MongoDB:', error.message)
   })
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context
-})
+const start = async () => {
+  const app = express()
+  const httpServer = http.createServer(app)
+  const schema = makeExecutableSchema({ typeDefs, resolvers })
+  const subscriptionServer = SubscriptionServer.create(
+    { schema, execute, subscribe, },
+    { server: httpServer, path: '', }
+  )
+  const plugins = [
+    ApolloServerPluginDrainHttpServer({ httpServer }), {
+      async serverWillStart () {
+        return { async drainServer () { subscriptionServer.close() }, }
+      },
+    },
+  ]
 
-server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`)
-})
+  const server = new ApolloServer({ schema, context, plugins })
+  await server.start()
+  server.applyMiddleware({ app, path: '/' })
+
+  const PORT = 4000
+  httpServer.listen(PORT, () =>
+    console.log(`Server is now running on http://localhost:${PORT}`)
+  )
+}
+
+start()
