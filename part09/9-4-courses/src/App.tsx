5fb33c2bc16206eeff8322b302cf67190239b125
Chris Hendrix <chendrix1123@gmail.com>
2022-04-24 19:11:26 -0400

    9.15

--- part09/9-4-courses/src/App.tsx
+++ part09/9-4-courses/src/App.tsx
@@ -1,13 +1,71 @@
-interface Course { name: string, exerciseCount: number }
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+  type: string;
+}
+interface CoursePartDescription extends CoursePartBase {
+  description: string
+}
+interface CourseNormalPart extends CoursePartDescription {
+  type: "normal";
+}
+interface CourseProjectPart extends CoursePartBase {
+  type: "groupProject";
+  groupProjectCount: number;
+}
+interface CourseSubmissionPart extends CoursePartDescription {
+  type: "submission";
+  exerciseSubmissionLink: string;
+}
+
+interface CourseSpecialPart extends CoursePartDescription {
+  type: "special";
+  requirements: Array<string>
+}
+
+type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart | CourseSpecialPart;
 
 const Header = ({ courseName }: { courseName: string }) => <><h1>{courseName}</h1></>
 
-const Content = ({ courseParts }:{ courseParts: Array<Course> }) => 
+const Part = ({ coursePart }: { coursePart: CoursePart }) => {
+  const renderList = () => {
+    switch(coursePart.type) {
+      case 'normal': 
+        return <>
+          <li>{coursePart.description}</li>
+        </>
+      case 'groupProject':
+        return <>
+          <li>{`project exercises ${coursePart.groupProjectCount}`}</li>
+        </>
+      case 'submission':
+        return <>
+          <li>{coursePart.description}</li>
+          <li>{`submit to ${coursePart.exerciseSubmissionLink}`}</li>
+        </>
+      case 'special':
+        return <>
+          <li>{coursePart.description}</li>
+          <li>{`required skills: ${coursePart.requirements.join(', ')}`}</li>
+        </>
+      default:
+        return <></>
+    }
+  }
+  return <>
+    <h3>{`${coursePart.name} ${coursePart.exerciseCount}`}</h3>
+    <ul>
+      {renderList()}
+    </ul>
+  </>
+}
+
+const Content = ({ courseParts }:{ courseParts: Array<CoursePart> }) => 
   <>
-    {courseParts.map(c => <p key={c.name}>{`${c.name} ${c.exerciseCount}`}</p>)}
+    {courseParts.map(c => <Part key={c.name} coursePart={c} />)}
   </>
 
-const Total = ({ courseParts }:{ courseParts: Array<Course> }) => 
+const Total = ({ courseParts }:{ courseParts: Array<CoursePart> }) => 
   <>
     <p>
       Number of exercises{" "}
@@ -17,20 +75,40 @@ const Total = ({ courseParts }:{ courseParts: Array<Course> }) =>
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is the leisured course part",
+      type: "normal"
+    },
+    {
+      name: "Advanced",
+      exerciseCount: 7,
+      description: "This is the harded course part",
+      type: "normal"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      type: "groupProject"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+      type: "submission"
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      type: "special"
     }
-  ];
+  ]
   return (
     <div>
       <Header courseName={courseName} />
