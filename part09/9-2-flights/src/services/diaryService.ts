939b136f7f6fc291696757c5b3697683dbd0ec0c
Chris Hendrix <chendrix1123@gmail.com>
2022-04-24 12:39:24 -0400

    part09c proofing

--- part09/9-2-flights/src/services/diaryService.ts
+++ part09/9-2-flights/src/services/diaryService.ts
@@ -1,5 +1,5 @@
 import diaries from '../../data/diaries';
-import { DiaryEntry, NonSensitiveDiaryEntry } from '../types';
+import { NewDiaryEntry, NonSensitiveDiaryEntry, DiaryEntry } from '../types';
 
 const getEntries = (): Array<DiaryEntry> => {
   return diaries;
@@ -14,12 +14,25 @@ const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
   }));
 };
 
-const addDiary = () => {
-  return null;
+const findById = (id: number): DiaryEntry | undefined=> {
+  const entry = diaries.find(d => d.id === id);
+  return entry;
 };
+
+const addDiary = ( entry: NewDiaryEntry ): DiaryEntry => {
+  const newDiaryEntry = {
+    id: Math.max(...diaries.map(d => d.id)) + 1,
+    ...entry
+  };
+
+diaries.push(newDiaryEntry);
+return newDiaryEntry;
+};
+
 
 export default {
   getEntries,
-  addDiary,
-  getNonSensitiveEntries
+  getNonSensitiveEntries,
+  findById,
+  addDiary
 };
