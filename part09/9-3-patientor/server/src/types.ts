fbb5cc51d7c40cc8f1db92d5670fb4a64ceb7cb5
Chris Hendrix <chendrix1123@gmail.com>
2022-04-24 11:18:21 -0400

    9.10

---
+++ part09/9-3-patientor/server/src/types.ts
@@ -0,0 +1,15 @@
+export type Gender = 'male' | 'female' | 'other';
+
+export interface Diagnosis {
+  code: string,
+  name: string,
+  latin?: string
+}
+
+export interface Patient {
+  id: string,
+  name: string,
+  dateOfBirth: Date,
+  ssn: string,
+  gender: Gender
+}
