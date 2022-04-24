617dd6ab8ca8250537ba5a773d9320182c2f12bb
Chris Hendrix <chendrix1123@gmail.com>
2022-04-24 18:07:27 -0400

    9.14

--- part09/9-4-courses/src/App.tsx
+++ part09/9-4-courses/src/App.tsx
@@ -1,36 +1,48 @@
-const App = () => {
-  const courseName = "Half Stack application development";
-  const courseParts = [
-    {
-      name: "Fundamentals",
-      exerciseCount: 10
-    },
-    {
-      name: "Using props to pass data",
-      exerciseCount: 7
-    },
-    {
-      name: "Deeper type usage",
-      exerciseCount: 14
-    }
-  ];
+const courseName = "Half Stack application development";
+const courseParts = [
+  {
+    name: "Fundamentals",
+    exerciseCount: 10
+  },
+  {
+    name: "Using props to pass data",
+    exerciseCount: 7
+  },
+  {
+    name: "Deeper type usage",
+    exerciseCount: 14
+  }
+];
 
+const Header = () => <><h1>{courseName}</h1></>
+
+const Content = () => 
+  <>
+    <p>
+      {courseParts[0].name} {courseParts[0].exerciseCount}
+    </p>
+    <p>
+      {courseParts[1].name} {courseParts[1].exerciseCount}
+    </p>
+    <p>
+      {courseParts[2].name} {courseParts[2].exerciseCount}
+    </p>
+  </>
+
+const Total = () => 
+  <>
+    <p>
+      Number of exercises{" "}
+      {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+    </p>
+  </>
+
+const App = () => {
   return (
     <div>
-      <h1>{courseName}</h1>
-      <p>
-        {courseParts[0].name} {courseParts[0].exerciseCount}
-      </p>
-      <p>
-        {courseParts[1].name} {courseParts[1].exerciseCount}
-      </p>
-      <p>
-        {courseParts[2].name} {courseParts[2].exerciseCount}
-      </p>
-      <p>
-        Number of exercises{" "}
-        {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
-      </p>
+      <Header />
+      <Content />
+      <Total />
     </div>
   );
 };
