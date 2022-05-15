e2f1c2e0f74d619edb90c66222835b0cec8c9e82
Chris Hendrix <chendrix1123@gmail.com>
2022-05-15 18:18:15 -0400

    9.19

--- part09/9-3-patientor/server/src/types.ts
+++ part09/9-3-patientor/server/src/types.ts
@@ -10,9 +10,43 @@ export interface Diagnosis {
   latin?: string
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: Date;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
 }
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+export type Discharge = { date: Date, criteria: string };
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+export type SickLeave = { startDate: Date, endDate: Date };
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave: SickLeave
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export interface Patient {
   id: string,
@@ -27,3 +61,8 @@ export interface Patient {
 export type PublicPatient = Omit<Patient, 'ssn' | 'entries' >;
 
 export type NewPatient = Omit<Patient, 'id'>;
+
+// Define special omit for unions
+export type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+// Define Entry without the 'id' property
+export type EntryWithoutId = UnionOmit<Entry, 'id'>;
--- part09/9-3-patientor/server/src/utils.ts
+++ part09/9-3-patientor/server/src/utils.ts
@@ -1,20 +1,16 @@
 import { v1 as uuid } from 'uuid';
-import { Patient, Gender } from "./types";
+import { Patient, Gender, Entry, Diagnosis, HealthCheckRating, Discharge, SickLeave } from './types';
 
 const isString = (text: unknown): text is string => {
   return typeof text === 'string' || text instanceof String;
 };
 
 const parseString = (comment: unknown): string => {
-  if (!comment || !isString(comment)) {
-    throw new Error('Incorrect or missing comment');
-  }
+  if (!comment || !isString(comment)) throw new Error('Incorrect or missing comment');
   return comment;
 };
 
-const isDate = (date: string): boolean => {
-  return Boolean(Date.parse(date));
-};
+const isDate = (date: string): boolean => Boolean(Date.parse(date));
 
 const parseDate = (date: unknown): Date => {
   if (!date || !isString(date) || !isDate(date)) {
@@ -30,14 +26,72 @@ const isGender = (param: any): param is Gender => {
 };
 
 const parseGender = (gender: unknown): Gender => {
-  if (!gender || !isGender(gender)) {
-      throw new Error('Incorrect or missing gender: ' + gender);
-  }
+  if (!gender || !isGender(gender)) throw new Error('Incorrect or missing gender');
   return gender;
 };
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isDiagnosisCodes = (param: any): param is Array<Diagnosis["code"]> => {
+  if (!Array.isArray(param)) return false;
+  if (!param.every((code: unknown) => isString(code))) return false;
+  return true;
+};
+
+const parseDiagnosisCodes = (diagnosisCodes: unknown): Array<Diagnosis["code"]> => {
+  if (!diagnosisCodes) return [];
+  if (!isDiagnosisCodes(diagnosisCodes)) throw new Error("Incorrect diagnosis codes format");
+  return diagnosisCodes;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isHealthCheckRating = (param: any): param is HealthCheckRating => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  return Object.values(HealthCheckRating).includes(param);
+};
+
+const parseHealthCheckRating = (rating: unknown): HealthCheckRating => {
+  if ( !isHealthCheckRating(rating)) throw new Error("Incorrect or missing health check rating");
+  return rating;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isDischarge = (param: any): param is Discharge => {
+  if (!param) return false;
+  /* eslint-disable @typescript-eslint/no-unsafe-argument */
+  if (!Object.keys(param).includes('date') || Object.keys(param).includes('criteria')) return false;
+  if (!isDate(param.date)) return false;
+  if (!isString(param.date)) return false;
+  /* eslint-enable @typescript-eslint/no-unsafe-argument */
+  return true;
+};
+
+const parseDischarge = (discharge: unknown): Discharge => {
+  if (!isDischarge(discharge)) throw new Error("Incorrect or missing discharge data");
+  const date = parseDate(discharge.date);
+  const criteria = parseString(discharge.criteria);
+  return { date, criteria };
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isSickLeave = (sickLeave: any): sickLeave is SickLeave => {
+  if (!sickLeave) return false;
+  /* eslint-disable @typescript-eslint/no-unsafe-argument */
+  if (!Object.keys(sickLeave).includes('startDate') || Object.keys(sickLeave).includes('endDate')) return false;
+  if (!isDate(sickLeave.startDate)) return false;
+  if (!isString(sickLeave.endDate)) return false;
+  /* eslint-enable @typescript-eslint/no-unsafe-argument */
+  return true;
+};
+
+const parseSickLeave = (sickLeave: any): SickLeave => {
+  if (!isSickLeave(sickLeave)) throw new Error("Incorrect or missing sick leave data");
+  const startDate = parseDate(sickLeave.startDate);
+  const endDate = parseDate(sickLeave.endDate);
+  return { startDate, endDate };
+};
+
 type PatientFields = { id: unknown, name: unknown, dateOfBirth: unknown, 
-  ssn: unknown, gender: unknown, occupation: unknown };
+  ssn: unknown, gender: unknown, occupation: unknown, entries: unknown };
 
 const createPatient = (
     {id, name, dateOfBirth, ssn, gender, occupation}: PatientFields
@@ -53,5 +107,43 @@ const createPatient = (
   };
   return patient;
 };
+
+type EntryFields = { id: unknown, type: unknown, description : unknown, date: unknown, specialist: unknown, 
+  diagnosisCodes?: unknown, healthCheckRating?: unknown, discharge?: unknown, employerName?: unknown, sickLeave?: unknown };
+
+  export const createEntry = ({ id, type, description, date, specialist, diagnosisCodes, 
+    healthCheckRating, discharge, employerName, sickLeave}: EntryFields): Entry | undefined | void => {
+    const baseEntry = {
+      id: id ? parseString(id) : uuid(),
+      description: parseString(description),
+      date: parseDate(date),
+      specialist: parseString(specialist),
+      diagnosisCodes: parseDiagnosisCodes(diagnosisCodes)
+    };
+  
+    switch (type) {
+      case "Hospital":
+        return {
+          ...baseEntry,
+          type: type,
+          discharge: parseDischarge(discharge),
+        };
+      case "HealthCheck":
+        return {
+          ...baseEntry,
+          type: type,
+          healthCheckRating: parseHealthCheckRating(
+            healthCheckRating
+          ),
+        };
+      case "OccupationalHealthcare":
+        return {
+          ...baseEntry,
+          type: type,
+          employerName: parseString(employerName),
+          sickLeave: parseSickLeave(sickLeave),
+        };
+    }
+  };
 
 export default createPatient;
