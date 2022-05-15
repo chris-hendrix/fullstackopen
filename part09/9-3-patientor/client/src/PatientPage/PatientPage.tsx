3285bf0ee6fff0396151780e9d735811c38201c6
Chris Hendrix <chendrix1123@gmail.com>
2022-05-15 19:56:02 -0400

    9.20

--- part09/9-3-patientor/client/src/PatientPage/PatientPage.tsx
+++ part09/9-3-patientor/client/src/PatientPage/PatientPage.tsx
@@ -1,8 +1,8 @@
 
-import { Patient } from '../types';
+import { Patient, Entry, Diagnosis } from '../types';
 import { useStateValue } from "../state";
 import { useParams } from 'react-router-dom';
-import { Typography } from "@material-ui/core";
+import { Box, Typography } from "@material-ui/core";
 
 import MaleIcon from '@mui/icons-material/Male';
 import FemaleIcon from '@mui/icons-material/Female';
@@ -17,6 +17,7 @@ const PatientPage = () => {
   const [{ patients }] = useStateValue();
   const { id } = useParams<{ id: string }>();
   const patient: Patient | null = id ? patients[id] : null;
+  const entries: Entry[] = patient && patient.entries ? patient.entries : [];
 
   return (
     <>
@@ -24,9 +25,23 @@ const PatientPage = () => {
         {patient?.name} {genderIconMap[patient?.gender || 'other']}
       </Typography>
       <Typography variant="body1" style={{ marginTop: "0.5em" }}>
-        {`date of birth: ${patient?.dateOfBirth || ''}`}
+        {`ssn: ${patient?.ssn || ''}`}
         <br/>
         {`occupation: ${patient?.occupation || ''}`}
+      </Typography>
+      <Typography variant="h5" style={{ marginTop: "0.5em" }}>
+        Entries
+      </Typography>
+      <Typography variant="body1" style={{ marginTop: "0.5em" }}>
+        {entries.map((e: Entry) => {
+          const codes: Array<Diagnosis['code']> = e.diagnosisCodes ? e.diagnosisCodes : [];
+          return (
+            <>
+              <Box key={e.id}>{`${e.date} - ${e.description}`}</Box>
+              {codes.map((c: Diagnosis['code']) => <li key={c}>{c}</li>)}
+            </>
+          );
+        })}
       </Typography>
     </>
 
--- part09/9-3-patientor/client/src/types.ts
+++ part09/9-3-patientor/client/src/types.ts
@@ -10,6 +10,20 @@ export enum Gender {
   Other = "other"
 }
 
+export interface Diagnosis {
+  code: string,
+  name: string,
+  latin?: string
+}
+
+export interface Entry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
 export interface Patient {
   id: string;
   name: string;
@@ -17,4 +31,6 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries?: Entry[]
 }
+
