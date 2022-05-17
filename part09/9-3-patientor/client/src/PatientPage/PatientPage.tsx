68b3c913064b456caae5d87e66a028771c892192
Chris Hendrix <chendrix1123@gmail.com>
2022-05-16 20:53:49 -0400

    9.21

--- part09/9-3-patientor/client/src/PatientPage/PatientPage.tsx
+++ part09/9-3-patientor/client/src/PatientPage/PatientPage.tsx
@@ -14,10 +14,17 @@ const genderIconMap = {
 };
 
 const PatientPage = () => {
-  const [{ patients }] = useStateValue();
+  const [{ patients, diagnoses }] = useStateValue();
   const { id } = useParams<{ id: string }>();
   const patient: Patient | null = id ? patients[id] : null;
   const entries: Entry[] = patient && patient.entries ? patient.entries : [];
+
+  const renderDiagnosis = (code: string): JSX.Element => {
+    const name: string = code in diagnoses ? diagnoses[code].name : 'no name available';
+    return (
+      <li key={code}>{`${code} - ${name}`}</li>
+    );
+  };
 
   return (
     <>
@@ -32,17 +39,19 @@ const PatientPage = () => {
       <Typography variant="h5" style={{ marginTop: "0.5em" }}>
         Entries
       </Typography>
-      <Typography variant="body1" style={{ marginTop: "0.5em" }}>
+      <Box style={{ marginTop: "0.5em" }}>
         {entries.map((e: Entry) => {
           const codes: Array<Diagnosis['code']> = e.diagnosisCodes ? e.diagnosisCodes : [];
           return (
-            <>
-              <Box key={e.id}>{`${e.date} - ${e.description}`}</Box>
-              {codes.map((c: Diagnosis['code']) => <li key={c}>{c}</li>)}
-            </>
+            <Box key={e.id}>
+              <Typography>
+                {`${e.date} - ${e.description}`}
+                {codes.map((c: Diagnosis['code']) => renderDiagnosis(c))}
+              </Typography>
+            </Box>
           );
         })}
-      </Typography>
+      </Box>
     </>
 
   );
