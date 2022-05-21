00dc8b91b4eda93507da088a5a8038c86f7910c6
Chris Hendrix <chendrix1123@gmail.com>
2022-05-21 12:44:45 -0400

    part13a other

--- part13/13-0-notes/index.js
+++ part13/13-0-notes/index.js
@@ -38,6 +38,7 @@ Note.init({
 
 app.get('/api/notes', async (req, res) => {
   const notes = await Note.findAll()
+  console.log(JSON.stringify(notes, null, 2))
   res.json(notes)
 })
 
@@ -47,6 +48,27 @@ app.post('/api/notes', async (req, res) => {
     return res.json(note)
   } catch (error) {
     return res.status(400).json({ error })
+  }
+})
+
+app.get('/api/notes/:id', async (req, res) => {
+  const note = await Note.findByPk(req.params.id)
+  if (note) {
+    console.log(note.toJSON())
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.put('/api/notes/:id', async (req, res) => {
+  const note = await Note.findByPk(req.params.id)
+  if (note) {
+    note.important = req.body.important
+    await note.save()
+    res.json(note)
+  } else {
+    res.status(404).end()
   }
 })
 
